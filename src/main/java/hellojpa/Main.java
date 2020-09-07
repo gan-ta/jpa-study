e8729a1eae85c128417ee87194f7a8b6a252ffdb
gnsta <gilmat@naver.com>
2020-09-07 22:55:41 +0900

    초기 세팅

--- src/main/java/hellojpa/Main.java
+++ src/main/java/hellojpa/Main.java
@@ -9,8 +9,10 @@ import javax.persistence.Persistence;
 
 public class Main {
     public static void main(String[] args){
+        //엔티티 매니저 팩토리는 하나만 생성해서 애플리케이션 전체에서 공유
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
+        //엔티티 메니저는 쓰레드간 공유하면 안됨
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         tx.begin();
