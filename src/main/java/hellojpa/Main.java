163afbeea779e764002f0196fbbdc89f04328cf2
gnsta <gilmat@naver.com>
2020-09-08 01:07:10 +0900

    연관관계 매핑

--- src/main/java/hellojpa/Main.java
+++ src/main/java/hellojpa/Main.java
@@ -2,11 +2,13 @@ package hellojpa;
 
 import entity.Member;
 import entity.MemberType;
+import entity.Team;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args){
@@ -18,14 +20,33 @@ public class Main {
 
 
         try {
+
+            //팀 저장
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            //멤버 저장
             Member member = new Member();
-//            member.setId(100L);
-            member.setName("안녕하세요");
-            member.setMemberType(MemberType.ADMIN);
-
-
+            member.setName("member1");
+            member.setTeam(team);
             em.persist(member);
+
+            em.flush();//쿼리 보냄
+            em.clear();//캐시 비움
+
             tx.commit();
+
+            Member findMember = em.find(Member.class,member.getId());
+            Team findTeam = findMember.getTeam();
+
+//            findTeam.getName();
+
+            List<Member> members = findTeam.getMembers();
+            for(Member member1: members){
+                System.out.println("member1 = " + member1);
+            }
+
         }catch (Exception e){
             tx.rollback();
         }finally {
