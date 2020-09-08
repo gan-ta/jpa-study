d7a2af7b701c0e49522b2c632156ae4b25098798
gnsta <gilmat@naver.com>
2020-09-08 15:22:10 +0900

    객체지향 쿼리

--- src/main/java/hellojpa/Main.java
+++ src/main/java/hellojpa/Main.java
@@ -14,6 +14,8 @@ public class Main {
     public static void main(String[] args){
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
+
+
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         tx.begin();
@@ -48,7 +50,5 @@ public class Main {
         }
         emf.close();
     }
-
-
 
 }
